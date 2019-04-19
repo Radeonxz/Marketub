6c01f436631d57db1aef1c87dcffbd0b38ab3b85
Radeonxz <xuanxxzhao@gmail.com>
2019-04-18 22:51:07 -0400

    cleaned up

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,7 +2,6 @@ const moduleName = 'logger.js';
 
 const moment = require('moment');
 const logger = (req, res, next) => {
-    // console.log(req);
     console.log(`${req.method}: ${req.protocol}://${req.get('host')}${req.originalUrl}, ${moment().format()}`);
     next();
 };
