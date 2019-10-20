6eab6c8c5ac224a477849593b5c1a94ffd7ffb5d
Readonxz <xuanxxzhao@gmail.com>
2019-10-19 23:43:57 -0400

    remove import

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/authActions';
 
-import Routes from './routes';
+// import Routes from './routes';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
