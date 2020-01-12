ea9bf97059eee95c85c243c51a4ba545dbc313db
Radeonxz <xuanxxzhao@gmail.com>
2020-01-12 11:49:30 -0500

    chore: add authProviderContainer

--- client/src/labs/containers/FilterList/index.js
+++ client/src/labs/containers/FilterList/index.js
@@ -1,11 +1,8 @@
 import { connect } from "react-redux";
-import { compose } from "redux";
 import FilterList from "./FilterList";
 
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({});
 
-const withConnect = connect(mapStateToProps, mapDispatchToProps);
-
-export default compose(withConnect)(FilterList);
+export default connect(mapStateToProps, mapDispatchToProps)(FilterList);
