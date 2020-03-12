08f5bbf70641e81ed3c3ce3d60ced043554d7544
Radeonxz <xuanxxzhao@gmail.com>
2020-03-11 23:48:33 -0400

    chore: disable file upload

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const moduleNam = 'server';
+const moduleNam = "server";
 const fctName = `${moduleNam}'s pid: ${process.pid}`;
 
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-global.__base = __dirname + '/';
-const config = require(path.join(__base, 'config/config'));
-const routes = require(path.join(__base, 'initializers/routes'));
-const logger = require(path.join(__base, 'middlewares/logger'));
+global.__base = __dirname + "/";
+const config = require(path.join(__base, "config/config"));
+const routes = require(path.join(__base, "initializers/routes"));
+const logger = require(path.join(__base, "middlewares/logger"));
 
 // Init express app setup
 const app = express();
-app.set('port', config.server.port);
-app.set('mongoURL', config.mongodb.url);
+app.set("port", config.server.port);
+app.set("mongoURL", config.mongodb.url);
 
 // Init static upload folder
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
 // Init body parser middleware
 app.use(express.json());
@@ -24,10 +24,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Method', 'POST, GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
@@ -42,24 +42,24 @@ routes.setup(app);
 // Routes validation
 const reditectUnmatchedAPI = (req, res) => {
   return res.status(400).json({
-    status: 'error',
+    status: "error",
     data: {
-      level: 'ERR',
-      code: '400',
-      message: 'Invalid API Call',
-      details: 'Invalid API Call'
+      level: "ERR",
+      code: "400",
+      message: "Invalid API Call",
+      details: "Invalid API Call"
     }
   });
 };
-app.all('*', reditectUnmatchedAPI);
+app.all("*", reditectUnmatchedAPI);
 
 // Serve static assets if in production
-if (config.node_env === 'production') {
+if (config.node_env === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
@@ -71,15 +71,15 @@ const MongoDBOptions = {
 };
 
 mongoose
-  .connect(app.get('mongoURL'), MongoDBOptions)
+  .connect(app.get("mongoURL"), MongoDBOptions)
   .then(() => {
     // Start server
-    console.info('Express server starting ....');
-    const server = app.listen(app.get('port'), function() {
+    console.info("Express server starting ....");
+    const server = app.listen(app.get("port"), function() {
       console.info(
-        'Express server is listening on port ' +
+        "Express server is listening on port " +
           server.address().port +
-          ', ' +
+          ", " +
           fctName
       );
     });
