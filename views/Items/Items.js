ee565308618e3cb2cd1facc093844023ef08d951
Radeonxz <xuanxxzhao@gmail.com>
2020-05-15 21:13:23 -0400

    chore: format items and users

--- views/Items/Items.js
+++ views/Items/Items.js
@@ -1,41 +1,40 @@
-const moduleName = 'Items.js';
+const moduleName = "Items.js";
 
-const path = require('path');
-const config = require(path.join(__base, 'config/config'));
+const path = require("path");
+const config = require(path.join(__base, "config/config"));
 
 // Item Model
-const Item = require(path.join(__base, 'models/Item'));
+const Item = require(path.join(__base, "models/Item"));
 
 // GetItems
 const getItems = (req, res) => {
   Item.find()
-  .sort({date: -1})
-  .then(items => res.json(items));
+    .sort({ date: -1 })
+    .then((items) => res.json(items));
 };
 exports.getItems = getItems;
 
 // GetItem
 const getItem = (req, res) => {
   Item.findById(req.params.id)
-  .then(item => res.json(item))
-  .catch(err => res.status(404).json({success: false}));
+    .then((item) => res.json(item))
+    .catch((err) => res.status(404).json({ success: false }));
 };
 exports.getItem = getItem;
-
 
 // PostItem
 const postItem = (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
-  newItem.save().then(item => res.json(item));
+  newItem.save().then((item) => res.json(item));
 };
 exports.postItem = postItem;
 
 // DeleteItem
 const deleteItem = (req, res) => {
   Item.findById(req.params.id)
-  .then(item => item.remove().then(() => res.json({success: true})))
-  .catch(err => res.status(404).json({success: false}));
+    .then((item) => item.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false }));
 };
 exports.deleteItem = deleteItem;
--- views/Users/Users.js
+++ views/Users/Users.js
@@ -1,28 +1,27 @@
-const moduleName = 'Users.js';
+const moduleName = "Users.js";
 
-const path = require('path');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require(path.join(__base, 'config/config'));
+const path = require("path");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require(path.join(__base, "config/config"));
 
 // User Model
-const User = require(path.join(__base, 'models/User'));
+const User = require(path.join(__base, "models/User"));
 
 // PostUsers
 const postUsers = (req, res) => {
   const { name, email, password } = req.body;
 
   // Validation
-  if(!name || !email || !password) {
-    config.err_400.data.message = 'Please enter all fields'
+  if (!name || !email || !password) {
+    config.err_400.data.message = "Please enter all fields";
     return res.status(400).json(config.err_400);
   }
 
   // Check for existing user
-  User.findOne({ email })
-  .then(user => {
-    config.err_400.data.message = 'User already exists'
-    if(user) return res.status(400).json(config.err_400);
+  User.findOne({ email }).then((user) => {
+    config.err_400.data.message = "User already exists";
+    if (user) return res.status(400).json(config.err_400);
 
     const newUser = new User({
       name,
@@ -33,14 +32,16 @@ const postUsers = (req, res) => {
     // Create salt & hash
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if(err) throw err;
+        if (err) throw err;
         newUser.password = hash;
-        newUser.save()
-        .then(user => {
+        newUser.save().then((user) => {
           jwt.sign(
             // Payload, secret, expiration, callback
-            { id: user.id }, config.JWTSecret, { expiresIn: 3600 }, (err, token) => {
-              if(err) throw err;
+            { id: user.id },
+            config.JWTSecret,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
               res.json({
                 token,
                 user: {
@@ -50,7 +51,7 @@ const postUsers = (req, res) => {
                 }
               });
             }
-          ); 
+          );
         });
       });
     });
